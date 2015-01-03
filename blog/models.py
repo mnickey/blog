c7ae00ab6d79db92f21cf298163ef33e9c995a96
Michael Nickey <mnickey@gmail.com>
2015-01-03 14:21:50 -0800

    Added models.py to have User

--- blog/models.py
+++ blog/models.py
@@ -10,4 +10,14 @@ class Post(Base):
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
 
+from flask.ext.login import UserMixin
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, Sequence("user_id_sequence"), primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
+
 Base.metadata.create_all(engine)
