d5567b351ac41a551d80d865a5aed73aa8d31b3c
Michael Nickey <mnickey@gmail.com>
2015-01-06 19:43:51 -0800

    Stylized login and add post, moved to navbar

--- blog/views.py
+++ blog/views.py
@@ -10,12 +10,10 @@ from flask.ext.login import login_required
 def posts(page=1, paginate_by=10):
     # Zero-indexed page
     page_index = page - 1
-
     count = session.query(Post).count()
 
     start = page_index * paginate_by
     end = start + paginate_by
-
     total_pages = (count - 1) / paginate_by + 1
     has_next = page_index < total_pages - 1
     has_prev = page_index > 0
@@ -23,14 +21,12 @@ def posts(page=1, paginate_by=10):
     posts = session.query(Post)
     posts = posts.order_by(Post.datetime.desc()) # change to desc for descending order
     posts = posts[start:end]
-
     return render_template("posts.html",
         posts=posts,
         has_next=has_next,
         has_prev=has_prev,
         page=page,
-        total_pages=total_pages
-    )
+        total_pages=total_pages )
 
 @app.route("/post/add", methods=["GET"])
 @login_required
